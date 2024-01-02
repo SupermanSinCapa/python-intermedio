118f219bc45a75869307711591056e42011a2074
SupermanSinCapa <abeltelle86@gmail.com>
2024-01-02 13:15:10 -0500

    lambdas 1

--- 03_lambdas.py
+++ 03_lambdas.py
@@ -1,2 +1,14 @@
 ### Lambdas ###
 
+#? Es una forma mucho mas simple de crear funciones, se utiliza mucho para crear funciones dentro de funciones
+
+sum_two_values = lambda first_value, second_value: first_value + second_value
+print (sum_two_values(2, 4))
+
+multiply_values = lambda  first_value, second_value: first_value * second_value - 3
+print(multiply_values(2, 4))
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+print(sum_three_values(5)(2, 4))
